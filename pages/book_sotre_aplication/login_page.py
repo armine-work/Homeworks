75ae19ee4bfdde0590c6e165b457eb919fdb9f15
armine-work <armine.kh.work@gmail.com>
2025-10-24 00:29:05 +0400

    added verification of Login page elemenets

--- pages/book_sotre_aplication/login_page.py
+++ pages/book_sotre_aplication/login_page.py
@@ -4,15 +4,53 @@ from pages.base_page import BasePage
 
 class LoginPage(BasePage):
     LOGIN_URL = "https://demoqa.com/login"
-    USERNAME_FIELD = (By.ID, 'userName')
-    PASSWORD_FIELD = (By.ID, 'password')
-    LOGIN_BUTTON = (By.ID, 'login')
-    LOGOUT_BUTTON = (By.XPATH, "//button[normalize-space()='Log out']")
+    LOGIN_PAGE_TITLE = (By.XPATH, "//h1[normalize-space()='Login']")
+    LOGIN_PAGE_TEXT = (By.XPATH, "//h2[normalize-space()='Welcome,']")
+    LOGIN_PAGE_TEXT_2 = (By.XPATH, "//h5[normalize-space()='Login in Book Store']")
+    USERNAME_TEXT = (By.XPATH, "//label[@id='userName-label']")
+    USERNAME_FIELD = (By.ID, "userName")
+    PASSWORD_TEXT = (By.XPATH, "//label[@id='password-label']")
+    PASSWORD_FIELD = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login")
+    NEW_USER_BUTTON = (By.ID, "newUser")
     ERROR_MESSAGE = (By.XPATH, "//p[@id='name']")
+
+    LOGOUT_BUTTON = (By.XPATH, "//button[normalize-space()='Log out']")
 
 
     def open_login_page(self):
         self.open(self.LOGIN_URL)
+
+    def is_login_title_displayed(self):
+        assert self.is_element_visible(LoginPage.LOGIN_PAGE_TITLE), "❌ Login page title was not displayed"
+        return True
+
+    def is_login_page_text_displayed(self):
+        assert self.is_element_visible(LoginPage.LOGIN_PAGE_TEXT), f"❌ Login page text is not displayed"
+        assert self.is_element_visible(LoginPage.LOGIN_PAGE_TEXT_2), f"❌ Login page text 2 is not displayed"
+        return True
+
+    def is_username_text_displayed(self):
+        assert self.is_element_visible(LoginPage.USERNAME_TEXT), f"❌ Username text is not displayed"
+        return True
+
+    def is_username_field_displayed(self):
+        assert self.is_element_visible(LoginPage.USERNAME_FIELD), f"❌ Username field is not displayed"
+        return True
+
+    def is_password_text_displayed(self):
+        assert self.is_element_visible(LoginPage.PASSWORD_TEXT), f"❌ Password text is not displayed"
+        return True
+
+    def is_password_field_displayed(self):
+        assert self.is_element_visible(LoginPage.PASSWORD_FIELD), f"❌ Password field is not displayed"
+        return True
+
+    def is_login_button_loaded(self):
+        return self.is_element_visible(self.LOGIN_BUTTON)
+
+    def is_new_user_button_loaded(self):
+        return self.is_element_visible(self.NEW_USER_BUTTON)
 
     def login(self, username, password):
         self.type_text(self.USERNAME_FIELD, username)
@@ -20,8 +58,7 @@ class LoginPage(BasePage):
         self.scroll_to_element(self.LOGIN_BUTTON)
         self.click(self.LOGIN_BUTTON)
 
-    def is_login_loaded(self):
-        return self.is_element_visible(self.LOGIN_BUTTON)
+
 
     def is_login_error_displayed(self):
         return self.is_element_visible(self.ERROR_MESSAGE)
